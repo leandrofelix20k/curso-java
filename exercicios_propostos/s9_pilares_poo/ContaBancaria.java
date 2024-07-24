830fc521b19be4c93b9fc055b8dae8b43b48fb96
Leandro Felix <leandro.almeida3029@gmail.com>
2024-07-24 10:45:54 -0300

    Update all

---
+++ exercicios_propostos/s9_pilares_poo/ContaBancaria.java
@@ -0,0 +1,55 @@
+package exercicios_propostos.s9_pilares_poo;
+
+public class ContaBancaria {
+    private long numero;
+    private String titular;
+    private double saldo;
+
+    public ContaBancaria(long numero, String titular, double depositoInicial) {
+        this.numero = numero;
+        this.titular = titular;
+        depositar(depositoInicial);
+    }
+
+    public long getNumero() {
+        return numero;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public void depositar(double valor){
+        if(valor <= 0.0) {
+            System.out.println("\nError: O valor a ser depositado tem que ser maior que $0,00");
+        } else{
+            this.saldo += valor;
+        }
+    }
+
+    public void sacar(double valor){
+        if(valor <= 0.0) {
+            System.out.println("\nError: O valor a ser sacado tem que ser maior que $0,00");
+        } else{
+            this.saldo -= valor + 5;
+        }
+    }
+
+    @Override
+    public String toString(){
+        return "Account "
+                + numero
+                + ", Holder: "
+                + titular
+                + ", Balance: $"
+                + String.format("%.2f", saldo);
+    }
+}
---
+++ exercicios_propostos/s9_pilares_poo/aplication.java
@@ -0,0 +1,52 @@
+package exercicios_propostos.s9_pilares_poo;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class aplication {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        ContaBancaria contaBancaria;
+        String nome, op;
+        double deposito, depositoInicial;
+        long numeroConta;
+        boolean initialDeposit;
+
+        System.out.print("Enter account number: ");
+        numeroConta = sc.nextInt();
+        System.out.print("Enter account holder: ");
+        sc.nextLine();
+        nome = sc.nextLine();
+
+        do {
+            System.out.print("Is there an initial deposit (y/n)? ");
+            op = sc.nextLine().toLowerCase();
+            initialDeposit = op.equals("y");
+        } while(!op.equals("y") && !op.equals("n"));
+
+        if(initialDeposit){
+            System.out.print("Enter initial deposit value: ");
+            depositoInicial = sc.nextDouble();
+            contaBancaria = new ContaBancaria(numeroConta, nome, depositoInicial);
+        } else{
+            contaBancaria = new ContaBancaria(numeroConta, nome, 0.0);
+        }
+
+        System.out.println("\nAccount data: ");
+        System.out.println(contaBancaria);
+
+        System.out.print("\nEnter a deposit value: ");
+        contaBancaria.depositar(sc.nextDouble());
+        System.out.println("Updated account data: ");
+        System.out.println(contaBancaria);
+
+        System.out.print("\nEnter a withdraw value: ");
+        contaBancaria.sacar(sc.nextDouble());
+        System.out.println("Updated account data: ");
+        System.out.println(contaBancaria);
+
+        sc.close();
+    }
+}
