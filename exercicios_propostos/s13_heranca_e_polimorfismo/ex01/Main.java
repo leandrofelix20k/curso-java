f00859abbcae320a6dcbefe5921ee0f61428ef9a
leandrofelix20k <leandro.almeida3029@gmail.com>
2025-06-17 15:31:08 -0300

    Up ex01 and ex02 on s13 package

--- exercicios_propostos/s13_heranca_e_polimorfismo/ex01/Main.java
+++ exercicios_propostos/s13_heranca_e_polimorfismo/ex01/Main.java
@@ -7,8 +7,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter ther number of employees: ");
         int numEmployess = sc.nextInt();
--- exercicios_propostos/s13_heranca_e_polimorfismo/ex02/Main.java
+++ exercicios_propostos/s13_heranca_e_polimorfismo/ex02/Main.java
@@ -9,8 +9,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter the number of products: ");
         int numProdutos = sc.nextInt();
